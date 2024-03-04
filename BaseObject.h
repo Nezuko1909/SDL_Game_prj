4310315fca31fa93ca6f7ecd47b73e3a56b89811
Nezuko1909 <kny1909.nezuko@gmail.com>
2024-03-04 16:02:19 +0700

    4/3/2024 Modified

--- BaseObject.h
+++ BaseObject.h
@@ -1,11 +1,11 @@
-#ifndef Base_Object_H
+#ifndef Base_Object_Function_H_
 
 #include "baseFunc.h"
 #include <iostream>
 
 SDL_Surface* Base_Load_Media(const char path[]) {
     SDL_Surface* Carry = NULL;
-    Carry = SDL_LoadBMP(path);
+    Carry = IMG_Load(path);
     if (Carry == NULL) {
         std::cout<<"Unable to load:\n"<<path<<"\nSDL Error:\n"<<SDL_GetError();
         return NULL;
--- main.cpp
+++ main.cpp
@@ -3,26 +3,32 @@
 #include "BaseObject.h"
 
 SDL_Surface* Background = NULL;
+void close_everything() {
+
+}
+void stay() {
+    bool quit = false;
+    while (!quit) { 
+        while (SDL_PollEvent(&g_event)) {
+            if (g_event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+}
 
 int main(int argc, char* argv[]) {
-    if (!init_video()) {
+    if (!init_video()) {  ///init and create window
         std::cout<<"Failed to initialize\n";
     } 
     else {
-        Background=Base_Load_Media("img_source/BGR.bmp");
+        Background=Base_Load_Media("img_source/ei1.png");
         if (Background!=NULL) {
             SDL_BlitSurface(Background, NULL, g_screenSurface, NULL);
             SDL_UpdateWindowSurface(g_window);
         }
         // get window stay up
-        bool quit = false;
-        while (!quit) {
-            while (SDL_PollEvent(&g_event)) {
-                if (g_event.type == SDL_QUIT) {
-                    quit = true;
-                }
-            }
-        }
+        stay();
     }
     return 0;
 }
