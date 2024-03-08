302197a1c3bf054a3d81f01f0c082ec383de7b4e
Nezuko1909 <kny1909.nezuko@gmail.com>
2024-03-08 09:15:57 +0700

    Branch dev

--- BaseObject.h
+++ BaseObject.h
@@ -9,7 +9,7 @@ public:
     ~BaseObject();
     void SetRect(const int &x, const int &y) {rect_.x=x; rect_.y=y; }
     SDL_Rect GetRect() const {return rect_; }
-    SDL_Texture* GetObject() {return p_Object; }
+    SDL_Texture* GetTexture() {return p_Object; }
 
     bool LoadImg(std::string path, SDL_Renderer* screen);
     void Render(SDL_Renderer* des, const SDL_Rect* clip = NULL);
